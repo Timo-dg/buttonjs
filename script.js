982203c6a0e296d15a599be8955dd35d0ae7e1d3
Timo de Gruiter <timo@EDGITNEW.local>
2021-12-09 12:06:43 +0100

    eerste commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var btn = document.createElement('button');
+btn.style.margin = '10px';
+btn.innerHTML = 'switch lights on';
+document.body.appendChild(btn);
+
+// schijf hier tussen je code
+
+
+
+// schijf hier tussen je code
