6edd36b05784b84ff6f8da2648da77e54a5eede0
Timo de Gruiter <timo@EDGITNEW.local>
2021-12-13 14:23:10 +0100

    opdracht afgerond

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 var btn = document.createElement('button');
-btn.style.margin = '10px';
-btn.innerHTML = 'switch lights on';
+btn.innerHTML = 'Turn lights on';
 document.body.appendChild(btn);
-
+document.body.style.backgroundColor = 'black'
 // schijf hier tussen je code
-
-
-
-// schijf hier tussen je code
+btn.onclick = function() {aanzetten()}
+function aanzetten() {
+    if (btn.innerHTML == "Turn lights off") {
+        btn.innerHTML = "Turn lights on"
+        document.body.style.backgroundColor = "black";
+        console.log("Lights off")
+    } else {
+        btn.innerHTML = "Turn lights off";
+        document.body.style.backgroundColor = "yellow"
+        console.log("Lights on")
+    }
+}
